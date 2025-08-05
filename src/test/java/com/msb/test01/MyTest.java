2a7abc8565d4b810ded67e8f9ca44e3d2e4cbdf1
15072601235 <74296612+15072601235@users.noreply.github.com>
2025-08-05 15:59:28 +0800

    Update MyTest.java

--- src/test/java/com/msb/test01/MyTest.java
+++ src/test/java/com/msb/test01/MyTest.java
@@ -6,25 +6,7 @@ import static org.junit.Assert.*;  // 导入断言方法
 
 public class MyTest {
     @Test
-    static void main(String[] args) throws ClassNotFoundException, SQLException { // throws异常是下面forName上alt+enter后生成的
-        // 加载driver驱动：
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        // 获取连接
-        String url = "jdbc:mysql://192.168.1.34:3306/blockchain?serverTimezone=Asia/Shanghai";
-        String username = "root";
-        String password = "hscizyq";
-        Connection conn = DriverManager.getConnection(url,username,password);
-        // 创建会话
-        Statement sta = conn.createStatement();
-        // 发送SQL
-        ResultSet rs1 = sta.executeQuery("select * from sys_user where user_name <= 40");
-        // 处理结果
-        while(rs1.next()){
-            System.out.println(rs1.getInt("dept_id")+"---"+rs1.getString("user_name"));
-        }
-        // 关闭资源
-        sta.close();
-        conn.close();
-        assertTrue(true);  // 需要导入 org.junit.Assert.*
+    public void testExample() {
+        assertTrue(true);  // 测试方法
     }
 }

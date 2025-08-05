ebaf5ded120ce46991ab290a1388a11511280c63
huang <2823868335@qq.com>
2025-08-05 10:40:51 +0800

    提交

---
+++ src/test/java/com/msb/test01/Test.java
@@ -0,0 +1,26 @@
+package com.msb.test01;
+
+import java.sql.*;
+
+public class Test {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException { // throws异常是下面forName上alt+enter后生成的
+        // 加载driver驱动：
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 获取连接
+        String url = "jdbc:mysql://192.168.1.34:3306/blockchain?serverTimezone=Asia/Shanghai";
+        String username = "root";
+        String password = "hscizyq";
+        Connection conn = DriverManager.getConnection(url,username,password);
+        // 创建会话
+        Statement sta = conn.createStatement();
+        // 发送SQL
+        ResultSet rs1 = sta.executeQuery("select * from sys_user where user_name <= 40");
+        // 处理结果
+        while(rs1.next()){
+            System.out.println(rs1.getInt("dept_id")+"---"+rs1.getString("user_name"));
+        }
+        // 关闭资源
+        sta.close();
+        conn.close();
+    }
+}
